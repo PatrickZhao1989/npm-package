a19f444ba16037132390523c7746a86fd339cf4e
Patrick Zhao <PatrickZhao@ssw.com.au>
2020-01-09 10:47:54 +1100

    inital commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+function helloWorld1(input:string): void{
+	console.log(`HelloWorld1 from ${input}`)
+}
+
+function helloWorld2(input:string): void{
+	console.log(`HelloWorld1 from ${input}`)
+}
+
+function helloWorld3(input:string): void{
+	console.log(`HelloWorld1 from ${input}`)
+}
+
+module.exports = {helloWorld1, helloWorld2, helloWorld3};
