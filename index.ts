a566f108d332890c756776b53db7bc02dc48643b
Patrick Zhao <patrick.zhao1989@outlook.com>
2020-01-11 16:56:44 +1100

    update entry points

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
-function helloWorld1(input:string): void{
+export function helloWorld1(input:string): void{
 	console.log(`HelloWorld1 from ${input}`)
 }
 
-function helloWorld2(input:string): void{
+export function helloWorld2(input:string): void{
 	console.log(`HelloWorld1 from ${input}`)
 }
 
-function helloWorld3(input:string): void{
+export function helloWorld3(input:string): void{
 	console.log(`HelloWorld1 from ${input}`)
 }
-
-module.exports = {helloWorld1, helloWorld2, helloWorld3};
